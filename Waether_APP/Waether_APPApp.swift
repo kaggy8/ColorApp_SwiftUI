1edb20c12a26ea4eace280b452e4a91569b2080a
Stanislav <kaggy8@ya.ru>
2023-02-11 15:08:12 +0300

    Initial Commit

---
+++ Waether_APP/Waether_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Waether_APPApp.swift
+//  Waether_APP
+//
+//  Created by Stanislav Demyanov on 11.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct Waether_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
